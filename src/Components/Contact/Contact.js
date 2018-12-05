bec7edc41dc4466892e6cb4129c426742525ec4a
Unknown <lucas.consejo@ynov.com>
2018-12-05 19:52:57 +0100

    update server.js & add firebase

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,61 @@
 import React, { Component } from 'react';
 import '../../css/Contact.css'
 import Navbar from '../../Components/Navbar/Navbar.js'
+import firebase from 'firebase'
+import Db from '../../Firebase/config.js'
 
 class Contact extends Component{
+    constructor(props){
+        super(props)
+        firebase.initializeApp(Db)
+        this.state = {
+            name :"",
+            email: "",
+            message: ""
+        }
+    }
+
+    sendMessage(){
+        if(this.state.name.length > 0 && this.state.email.length > 0 && this.state.message.length > 0){
+            const ref = firebase.database().ref('msgbox/'+this.state.name)
+            ref.set({
+                name: this.state.name,
+                email:this.state.email,
+                message: this.state.message
+            }).then(()=>{
+                console.log("--> NEW MESSAGE")
+            }).catch((error)=>{
+                console.log("Err ",error )
+            })
+            this.setState({
+                name: "",
+                email: "",
+                message: ""                
+            })
+        }
+        else{
+            console.log('oupps...')
+        }
+    }
+
+    getName(event){
+        this.setState({
+            name:event.target.value
+        })
+    }
+
+    getEmail(event){
+        this.setState({
+            email:event.target.value
+        })
+    }
+
+    getMessage(event){
+        this.setState({
+            message:event.target.value
+        })
+    }
+
     render(){
         return(
             <div id="contact">
@@ -32,26 +85,26 @@ class Contact extends Component{
                                 <div className="col-lg-12">
                                     <p className="font-weight-bold text-light">Nom</p>
                                     <div className="input-group mb-3">
-                                        <input type="text" className="form-control" aria-label="Message" aria-describedby="inputGroup-sizing-default" />
+                                        <input type="text" value={this.state.name} onChange={this.getName.bind(this)} className="form-control" aria-label="Message" aria-describedby="inputGroup-sizing-default" />
                                     </div>
                                 </div>
 
                                 <div className="col-lg-12">
                                     <p className="font-weight-bold text-light">Adresse mail</p>
                                     <div className="input-group">
-                                        <input type="email" className="form-control" aria-describedby="email"/>
+                                        <input type="email" value={this.state.email} onChange={this.getEmail.bind(this)} className="form-control" aria-describedby="email"/>
                                     </div>
                                 </div>
 
                                 <div className="col-lg-12">
                                     <p className="font-weight-bold text-light">Message</p>
                                     <div className="input-group mb-3">
-                                        <textarea className="form-control" rows="3"></textarea>
+                                        <textarea className="form-control" value={this.state.message} onChange={this.getMessage.bind(this)} rows="3"></textarea>
                                     </div>
                                 </div>
 
                                 <div className="col-lg-12">
-                                    <button className="btn btn-light" type="button">Envoyer</button>
+                                    <button className="btn btn-light" type="button" onClick={this.sendMessage.bind(this)}>Envoyer</button>
                                 </div>
                             </div>
                         </div>
---
+++ src/Firebase/config.js
@@ -0,0 +1,7 @@
+var config = {
+    apiKey: "AIzaSyBCbPw4vP_c_N8uPoZc_rRf1gzxtdbf5Ok",
+    authDomain: "website-lucas.firebaseapp.com",
+    databaseURL: "https://website-lucas.firebaseio.com",
+  };
+
+export default config
