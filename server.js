db1c99f1502adb64dec52e43158d2711c3031a57
Unknown <lucas.consejo@ynov.com>
2018-12-07 00:17:47 +0100

    Redaction README.md

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(80);
+app.listen(80, () =>{
+  console.log('Le serveur vient de se lancer et écoute sur le port 80')
+})
