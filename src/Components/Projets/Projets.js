c4568ec1422e749521ba2969061b4a841a733f41
Unknown <lucas.consejo@ynov.com>
2018-12-06 23:52:32 +0100

    Add new Projet récent

--- src/Components/Projets/Projets.js
+++ src/Components/Projets/Projets.js
@@ -13,6 +13,18 @@ class Projets extends Component{
                     <div id="box-size" className="container">
                         <h2 className="mt-3 text-light">Projets récents</h2>
                         <div id="list-project" className="row mt-3 mx-auto mt-4">
+                        <a href="https://github.com/lucasconsejo/Website-Lucas" id="img-wetube" className="col-lg-4  col-12  text-center  text-dark  box" target="_blank" rel="noopener noreferrer">
+                                <div className="projet p-3">
+                                    <h5>Lucas Consejo - Développeur Web & Android</h5>
+                                    <img src={LogoWetube} className="img-rotation" alt="appli WeTube"/>
+                                    <p>J'ai développé mon site web avec React JS. J'ai utilisé Express pour le serveur web, que j'ai ensuite hébergé chez OVH sous Debian.</p>
+                                    <p>React js & Bootstrap</p>
+                                    <button className="btn btn-dark">  
+                                        Git du projet
+                                    </button >
+                                </div>
+                            </a>
+
                             <a href="https://github.com/lucasconsejo/Projects-React-js/tree/master/Movies%20Trailer" id="img-wetube" className="col-lg-4  col-12  text-center  text-dark  box" target="_blank" rel="noopener noreferrer">
                                 <div className="projet p-3">
                                     <h3>WeTube</h3>
@@ -24,12 +36,6 @@ class Projets extends Component{
                                     </button >
                                 </div>
                             </a>
-
-                            <div id="projet-coming" className="col-lg-4  col-12  text-center  box">
-                                <div className="projet p-3 projet-coming">
-                                    <p className="font-weight-bold">Indisponible</p>
-                                </div>
-                            </div>
 
                             <div id="projet-coming" className="col-lg-4  col-12  text-center  box">
                                 <div className="projet p-3 projet-coming">
