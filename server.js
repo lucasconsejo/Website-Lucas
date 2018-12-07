0cc6c90e1b93cc70f992bd8dccb6ef9a4727f1a3
Unknown <lucas.consejo@ynov.com>
2018-12-07 23:07:26 +0100

    slight update

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const https = require('https')
-const fs = require('fs')
 const path = require('path');
 const app = express();
 
@@ -10,11 +8,6 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const httpsOptions = {
-  cert:fs.readFileSync(path.join(__dirname, 'ssl', 'server.cer')),
-  key:fs.readFileSync(path.join(__dirname, 'ssl', 'server.key'))
-}
-
-https.createServer(httpsOptions, app).listen(80, () =>{
+app.listen(80, () =>{
   console.log('Le serveur vient de se lancer et écoute sur le port 80')
 })
