fc8e6f633c364b81c6636bd6bdffd8cbc10df2da
Unknown <lucas.consejo@ynov.com>
2018-12-08 23:00:46 +0100

    last update for sitemap

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const httpsOptions = {
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/sitemap.xml', (req, res) => (
+  res.status(200).sendFile('sitemap.xml')
+));
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
