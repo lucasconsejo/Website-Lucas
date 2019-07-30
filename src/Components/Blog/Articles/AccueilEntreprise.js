e7ec7d52d1cc6d403783e901c2b08b8379ae221d
lucas.consejo <lucas.consejo@cdbdx.com>
2019-07-30 18:11:58 +0200

    update article accueil-integration

--- src/Components/Blog/Articles/AccueilEntreprise.js
+++ src/Components/Blog/Articles/AccueilEntreprise.js
@@ -24,24 +24,100 @@ class AccueilEntreprise extends Component{
                         </div>
 
                         <div id="blog-part-1">
-                            <h3>I. Les premiers jours</h3>
+                            <h3>I. Le premier jour</h3>
 
-                            <p>Has autem provincias, quas Orontes ambiens amnis imosque pedes Cassii montis illius celsi praetermeans funditur in Parthenium mare, Gnaeus Pompeius superato Tigrane regnis Armeniorum abstractas dicioni Romanae coniunxit.
-                                Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.
-                                <br />Nunc vero inanes flatus quorundam vile esse quicquid extra urbis pomerium nascitur aestimant praeter orbos et caelibes, nec credi potest qua obsequiorum diversitate coluntur homines sine liberis Romae.
+                            <p>
+                                Je suis arrivé le Lundi 1er Juillet 2019 à 9h30 et la première étape était d'aller me présenter à l'accueil. J'étais loin d'être le seul arrivant au sein de Cdiscount. J'ai donc attendu ~15min dans la fil d'attente.
+                                Une fois l'attente terminée, on m'a remis mon badge d'entrée, un cahier de note, et...<span id="color-detail">une boîte de bienvenue</span> dans l'entreprise avec plein de petits cadeaux. &#128522;
+                                <br />
+                                <br />
+                                Ensuite, j'ai été accueilli par <span id="color-detail">Isabelle Cardoso</span> (<span id="color-detail">Chef de projet</span>) et elle m'a accompagné à mon nouveau bureau des 2 prochains mois. Direction donc un grand open-space nommé "Le cloud", où j'ai rencontré ma nouvelle équipe. &#128578;
+                                <br />
+                                <span id="color-detail">Stéphane Giauque</span> était aussi présent (j'ai eu mon entretien avec lui et c'est le <span id="color-detail">Directeur métiers IT</span>). Il m'a fourni mon ordinateur portable. J'ai ensuite du installer le matériel à mon bureau, à savoir : 
+                               <ul>
+                                    <li>Connecter mon ordi portable à un dock</li>
+                                    <li>Brancher les 2 écrans supplémentaires</li>
+                                    <li>Brancher le clavier / la souris / le casque audio.</li>
+                                </ul>
+                                Bien, mon poste est installé ! Me voilà donc avec 3 écrans ! Place maintenant à l'installation des logiciels dont j'aurais besoin pour mes futurs missions ainsi que récupérer mes identifiants pour les mails et les différents sites/plateformes de Cdiscount en interne.
+                                <br />
+                                <br />
+                                Parmis tout ces outils, les plus importants sont :
+                                <ul>
+                                    <li><a href="https://products.office.com/fr-fr/microsoft-teams/group-chat-software" target="_blank" className="previous-page">Teams</a> / <a href="https://www.skype.com/fr/" target="_blank" className="previous-page">Skype</a> pour communiquer entre chaque personne de l'équipe mais aussi avec d'autres personnes de d'autres service.</li>
+                                    <li><a href="https://outlook.live.com/owa/" target="_blank" className="previous-page">Outlook</a> pour les mails</li>
+                                    <li><a href="https://www.jetbrains.com/idea/" target="_blank" className="previous-page">IntelliJ</a> (dans mon cas) qui est un IDE pour pouvoir programmer en Java.</li>
+                                    <li><a href="https://visualstudio.microsoft.com/fr/tfs/" target="_blank" className="previous-page">TFS</a> qui fait office de server git au sein de Cdiscount.</li>
+                                    <li><a href="https://www.atlassian.com/fr/software/confluence" target="_blank" className="previous-page">Confluence</a> (pas important mais très utile) pour trouver differentes documentations pour aider sur différents projets.</li>
+                                </ul>
+
+                                Parfait ! Je suis prêt pour commencer ma première mission. &#128526;
                             </p>
                         </div>
 
                         <div id="blog-part-2">
-                           
+                            <h3>II. Présentation de ma première mission</h3>
+
+                            <p>
+                                <span id="color-detail">Isabelle</span> m'a donc présenté ma première mission de ce mois-ci. Mais je ne vais pas trop m'attarder dessus ici, un article y sera dédié.
+                                <br />
+                                Durant ce mois, je vais devoir migrer un <span id="color-detail">microservice</span> qui est sur Mésos/Marathon, vers <span id="color-detail">Kubernetes</span>. Le microservice (son petit nom est "<span id="color-detail">middle-search-loader-offer</span>") est sous Java 8. Avant de passer à la migration vers Kubernetes, il faut d'abord passer de Java 8 à Java 10.
+                                <br />
+                                <br />
+                                Vous n'avez pas tout compris ? <i>Pas de panique !</i> &#128516; Je vous invite à aller voir l'article dédié à ce sujet. <Link to="/blog/stage/cdiscount/article/mission-effectuees-l-aspect-technique" className="previous-page">Mission effectuées - l'aspect technique</Link>
+                            </p>
                         </div>
 
                         <div id="blog-part-3">
-                            
+                            <h3>III. Présentation du service / de l'équipe</h3>
+
+                            <span id="color-detail"><i>[A compléter...]</i></span>
                         </div>
 
                         <div id="blog-part-4">
-                            
+                            <h3>IV. Intégration dans l'équipe</h3>
+                            <br />
+                            <ul>
+                                <li>
+                                    <h5>L'aspect travail</h5>
+                                    <p>
+                                        Avant de se lancer complétement dans la mission, il faut prendre connaissance du contexte, du pourquoi on fait ça, où se situe le microservice par rapport aux autres, quelle base de données est appélée, etc...
+                                        <br />
+                                        Pour ça, il y a <span id="color-detail">Confluence</span> ! Il y a de la documentation écrite par les internes de Cdiscoun, pour presque tout. J'ai donc passé un peu de temps à lire tout ça, en savoir plus sur le contexte global chez Cdiscount,
+                                        comment sont reliées les différentes base de données, etc...
+                                        <br />
+                                        Mais le gros plus, et pas uniquement pour la doc, mais aussi pour la mission et autres => Si vous êtes bloqués ou perdu, il y aura toujours quelqu'un pour vous aider !
+                                    </p>
+                                </li>
+                                <li>
+                                    <h5>L'aspect social</h5>
+                                    <p>
+                                        Toutes les personnes avec qui j'ai eu l'occasion de parler, sont très souriante, gentille. Il y a une bonne ambiance et tout le monde s'entend bien avec tout le monde. &#128522;
+                                        <br/>
+                                        Le midi, il y a plusieurs foodtruck qui viennent se placer autour du bâtiment, mais pas loin de Cdiscount, il y a <i><span id="color-detail">Les Halles</span></i>. Là-bas, on peut y manger de tout. C'est un peu Le lieu où manger le midi entre collègue.
+                                        Vous y croiserez toujours quelqu'un que vous connaissez de votre équipe et de d'autres équipes, donc c'est très peu probable que vous vous retrouviez seul le midi. &#128523;
+                                        <br />
+                                        Un soir, <span id="color-detail">Isabelle</span> a proposé à l'équipe IT4IT, une soirée entre collègues <i><span id="color-detail">aux Halles</span></i>, pour souhaiter la bienvenue à tous les nouveaux. C'était très sympa et il y a comme toujours une bonne ambiance. &#128522;
+                                    </p>
+                                </li>
+                                <li>
+                                    <h5>L'aspect entreprise</h5>
+                                    <p>
+                                        Cdiscount est une grande entreprise. Il y a plusieurs outils à prendre en main, différents bâtiments, etc... Pour m'y retrouver, on m'a inscrit à une <span id="color-detail">formation</span> durant une après-midi pour la gestion des outils du SI. J'en ai donc appris plus sur le fonctionnement de Cdiscount en interne.
+                                        <br />
+                                        J'ai aussi rencontré <i>mon parrain au sein de Cdiscount</i> <span id="color-detail">Fabien Rouget</span> qui m'a fait une visite des locaux pour mieux m'y retrouver.
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div id="blog-part-5">
+                            <h3><i>Bonus : Unboxing de la boîte de bienvenue</i></h3>
+
+                            Si vous lisez ici, c'est que vous êtes curieux et avez grandement envie de savoir ce que contient cette fameuse boîte de bienvenue... &#128527;
+                            <br />
+                            <br />
+                            <span id="color-detail"><i>[A compléter...]</i></span>
                         </div>
                     </div>
 
